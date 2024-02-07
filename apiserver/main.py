5aa1e2778a2f6e5748b4d0bebc3618bae1b1a42f
sgrtye <sgrtye@gmail.com>
2024-02-07 02:07:30 +0000

    change exchange rate provider to yfinance

--- apiserver/main.py
+++ apiserver/main.py
@@ -16,14 +16,11 @@ XUI_PASSWORD = os.environ.get("XUI_PASSWORD")
 XUI_LOGIN_URL = os.environ.get("XUI_LOGIN_URL")
 XUI_STATUS_URL = os.environ.get("XUI_STATUS_URL")
 
-EXCHANGE_RATE_URL = os.environ.get("EXCHANGE_RATE_URL")
-
 if (
     XUI_USERNAME is None
     or XUI_PASSWORD is None
     or XUI_LOGIN_URL is None
     or XUI_STATUS_URL is None
-    or EXCHANGE_RATE_URL is None
 ):
     print("Environment variables not fulfilled")
 
@@ -32,7 +29,9 @@ exchange_status = None
 stock_status = None
 
 xui_session = requests.Session()
-tickers = yfinance.Tickers("^IXIC ^GSPC ^HSI 000001.SS")
+tickers = yfinance.Tickers(
+    "^IXIC ^GSPC ^HSI 000001.SS GBPCNY=X EURCNY=X CNY=X CADCNY=X"
+)
 
 
 class apiHandler(http.server.BaseHTTPRequestHandler):
@@ -67,7 +66,14 @@ def start_api_server():
         httpd.serve_forever()
 
 
-def formatBytes(bytes):
+def format_number(number):
+    if number == 0:
+        return "0"
+
+    return f"{number:.2f}".rstrip("0").rstrip(".")
+
+
+def format_bytes(bytes):
     if bytes == 0:
         return "0 Byte"
 
@@ -76,11 +82,11 @@ def formatBytes(bytes):
 
     i = math.floor(math.log(bytes) / math.log(k))
 
-    return f"{bytes / math.pow(k, i):.2f}".rstrip("0").rstrip(".") + " " + sizes[i]
+    return format_number(bytes / math.pow(k, i)) + " " + sizes[i]
 
 
-def bytesToSpeed(bytes):
-    return formatBytes(bytes) + "/s"
+def bytes_to_speed(bytes):
+    return format_bytes(bytes) + "/s"
 
 
 def update_xui_status():
@@ -101,52 +107,48 @@ def update_xui_status():
 
     if response.status_code == 200:
         response = response.json()
-        xui_status["up"] = bytesToSpeed(response["obj"]["netIO"]["up"])
-        xui_status["down"] = bytesToSpeed(response["obj"]["netIO"]["down"])
-        xui_status["sent"] = formatBytes(response["obj"]["netTraffic"]["sent"])
-        xui_status["recv"] = formatBytes(response["obj"]["netTraffic"]["recv"])
-
-
-def convert_exchange_rate(rate):
-    if rate == 0:
-        return "0"
-
-    return f"{1 / rate:.2f}".rstrip("0").rstrip(".")
+        xui_status["up"] = bytes_to_speed(response["obj"]["netIO"]["up"])
+        xui_status["down"] = bytes_to_speed(response["obj"]["netIO"]["down"])
+        xui_status["sent"] = format_bytes(response["obj"]["netTraffic"]["sent"])
+        xui_status["recv"] = format_bytes(response["obj"]["netTraffic"]["recv"])
 
 
 def update_exchange_status():
     global exchange_status
 
-    response = requests.get(EXCHANGE_RATE_URL)
+    try:
+        USD = tickers.tickers["CNY=X"].history(period="2d", interval="60m")
+        GBP = tickers.tickers["GBPCNY=X"].history(period="2d", interval="60m")
+        EUR = tickers.tickers["EURCNY=X"].history(period="2d", interval="60m")
+        CAD = tickers.tickers["CADCNY=X"].history(period="2d", interval="60m")
 
-    if response.status_code == 200:
-        response = response.json()
         exchange_status = {
-            "GBP": convert_exchange_rate(response["rates"]["GBP"]),
-            "EUR": convert_exchange_rate(response["rates"]["EUR"]),
-            "USD": convert_exchange_rate(response["rates"]["USD"]),
-            "CAD": convert_exchange_rate(response["rates"]["CAD"]),
+            "GBP": format_number(GBP["Close"][GBP["Close"].keys().max()]),
+            "EUR": format_number(EUR["Close"][EUR["Close"].keys().max()]),
+            "USD": format_number(USD["Close"][USD["Close"].keys().max()]),
+            "CAD": format_number(CAD["Close"][CAD["Close"].keys().max()]),
         }
-
-
-def convert_stock_price(price):
-    return f"{price:.2f}".rstrip("0").rstrip(".")
+    except Exception as e:
+        pass
 
 
 def update_stock_status():
     global stock_status
 
-    IXIC = tickers.tickers["^IXIC"].history(period="2d", interval="60m")
-    GSPC = tickers.tickers["^GSPC"].history(period="2d", interval="60m")
-    HSI = tickers.tickers["^HSI"].history(period="2d", interval="60m")
-    SS = tickers.tickers["000001.SS"].history(period="2d", interval="60m")
+    try:
+        HSI = tickers.tickers["^HSI"].history(period="2d", interval="60m")
+        IXIC = tickers.tickers["^IXIC"].history(period="2d", interval="60m")
+        GSPC = tickers.tickers["^GSPC"].history(period="2d", interval="60m")
+        SS = tickers.tickers["000001.SS"].history(period="2d", interval="60m")
 
-    stock_status = {
-        "IXIC": convert_stock_price(IXIC["Close"][IXIC["Close"].keys().max()]),
-        "GSPC": convert_stock_price(GSPC["Close"][GSPC["Close"].keys().max()]),
-        "HSI": convert_stock_price(HSI["Close"][HSI["Close"].keys().max()]),
-        "SS": convert_stock_price(SS["Close"][SS["Close"].keys().max()]),
-    }
+        stock_status = {
+            "SS": format_number(SS["Close"][SS["Close"].keys().max()]),
+            "HSI": format_number(HSI["Close"][HSI["Close"].keys().max()]),
+            "IXIC": format_number(IXIC["Close"][IXIC["Close"].keys().max()]),
+            "GSPC": format_number(GSPC["Close"][GSPC["Close"].keys().max()]),
+        }
+    except Exception as e:
+        pass
 
 
 if __name__ == "__main__":
@@ -154,7 +156,7 @@ if __name__ == "__main__":
     api_thread.daemon = True
     api_thread.start()
 
-    schedule.every().hour.do(update_exchange_status)
+    schedule.every(30).minutes.do(update_exchange_status)
     schedule.every(30).minutes.do(update_stock_status)
 
     schedule.run_all()
