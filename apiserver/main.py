eb3d70ec10e685ee1952e8ef9f8501faf7068de9
sgrtye <sgrtye@gmail.com>
2024-02-15 10:01:14 +0000

    remove warning handling

--- apiserver/main.py
+++ apiserver/main.py
@@ -10,10 +10,6 @@ import threading
 import http.server
 import socketserver
 
-import warnings
-
-warnings.simplefilter(action="ignore", category=FutureWarning)
-
 XUI_USERNAME = os.environ.get("XUI_USERNAME")
 XUI_PASSWORD = os.environ.get("XUI_PASSWORD")
 
