80416a96f36043dd2665e380f38608f4837b4c75
sgrtye <sgrtye@gmail.com>
2024-09-21 20:03:38 +0100

    refactoring nginx config update

--- submanager/update_nginx_config.py
+++ submanager/update_nginx_config.py
@@ -1,60 +1,22 @@
 import os
-import json
-import requests
 import datetime
+from xui_credentials import get_credentials
 
 DIRECTORY_PATH = "/sub"
 NGINX_PATH = "/conf.d"
 
-XUI_USERNAME = os.environ.get("XUI_USERNAME")
-XUI_PASSWORD = os.environ.get("XUI_PASSWORD")
-
-XUI_URL = os.environ.get("XUI_URL")
-
-if XUI_URL is None is None or XUI_USERNAME is None or XUI_PASSWORD is None:
-    print("Environment variables not fulfilled")
-
-
-def get_credentials():
-    session = requests.Session()
-    session.post(
-        XUI_URL + "/login", data={"username": XUI_USERNAME, "password": XUI_PASSWORD}
-    )
-    response = session.post(XUI_URL + "/panel/inbound/list")
-
-    if response.status_code != 200:
-        return None
-
-    results = []
-    for inbound in response.json()["obj"]:
-        info = {
-            "port": str(inbound["port"]),
-            "path": json.loads(inbound["streamSettings"])["wsSettings"]["path"][1:],
-        }
-        results.append(info)
-
-    return results
-
 
 def update_nginx_config(credentials):
     with open(
-        os.path.join(DIRECTORY_PATH, "file", "inbound.conf"), "r", encoding="utf-8"
-    ) as file:
-        inbound_template = file.read()
-
-    inbound = ""
-    for client in credentials:
-        port, path = client["port"], client["path"]
-        tmp = inbound_template.replace("PATH", path)
-        tmp = tmp.replace("PORT", port)
-        inbound = inbound + "\n\n" + tmp
-
-    with open(
         os.path.join(DIRECTORY_PATH, "file", "nginx.conf"), "r", encoding="utf-8"
     ) as file:
-        nginx_template = file.read()
+        config = file.read()
 
-    config = nginx_template.replace("INBOUNDS", inbound)
+    for client in credentials:
+        port, path, remark = client["port"], client["path"], client["remark"]
+        config = config.replace(f"{remark}_PORT", port)
+        config = config.replace(f"{remark}_PATH", path)
+
     with open(os.path.join(NGINX_PATH, "nginx.conf"), "w", encoding="utf-8") as file:
         file.write(config)
 
@@ -67,10 +29,6 @@ def update_nginx_config(credentials):
 def update():
     try:
         credentials = get_credentials()
-
-        if credentials is None:
-            raise Exception("No credentials available")
-
         update_nginx_config(credentials)
 
     except Exception as e:
