4ab697fbe98ea21727578d11f60b905a57c42ec8
sgrtye <sgrtye@gmail.com>
2025-01-11 21:48:53 +0000

    change log path

--- shadowgate/constants.py
+++ shadowgate/constants.py
@@ -2,4 +2,4 @@ MITCE_SHADOWROCKET_PATH = "/conf/mitce/shadowrocket"
 MITCE_CLASH_PATH = "/conf/mitce/clash.yaml"
 MITCE_CLASH_USERINFO_PATH = "/conf/mitce/userinfo.txt"
 
-CONFIG_ACCESS_LOG_PATH = "/log/config_access.log"
+CONFIG_ACCESS_LOG_PATH = "/logs/config_access.log"
